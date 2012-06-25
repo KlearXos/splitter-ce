a3203a83d19b2bc5987e43ca472c93408236371d
gation@gmail.com <gation@gmail.com@2015a741-d1ce-2762-4a5a-08cd768889d9>
2012-06-25 19:13:51 +0000

    Started branch with development from scratch + split driver with stubbed entry points

---
+++ serial_splitter/split_drv/driver_entry.h
@@ -0,0 +1,8 @@
+#ifndef __SPLIT_DRIVER_ENTRY_H__
+#define __SPLIT_DRIVER_ENTRY_H__
+
+// XXX: FILE HEAD
+
+#define DRIVER_FUNCTION		__declspec(dllexport)
+
+#endif
---
+++ serial_splitter/split_drv/driver_main.cpp
@@ -0,0 +1,16 @@
+// file just defines DLL main function
+
+#include <windows.h>
+
+BOOL APIENTRY
+DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls((HMODULE)hModule);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return (TRUE);
+}

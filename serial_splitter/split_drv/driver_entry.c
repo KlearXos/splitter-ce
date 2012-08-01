44e82c53c7f0e71e38ccd625b07f33d28919010a
gation@gmail.com <gation@gmail.com@2015a741-d1ce-2762-4a5a-08cd768889d9>
2012-08-01 18:34:59 +0000

    Screw the C++ architecture. It bring unnecessary complication.

--- serial_splitter/split_drv/driver_entry.c
+++ serial_splitter/split_drv/driver_entry.c
@@ -4,69 +4,64 @@
 
 #include "driver_entry.h"
 
+
 DRIVER_FUNCTION HANDLE
 COM_Init(ULONG Identifier) {
-	// XXX: not implemented
 	return NULL;
 }
 
 DRIVER_FUNCTION BOOL
 COM_PreDeinit(PVOID pInitContext) {
-	// XXX: not implemented
 	return FALSE;
 }
 
 DRIVER_FUNCTION BOOL
 COM_Deinit(PVOID pInitContext) {
-	// XXX: not implemented
 	return FALSE;
-}
-
-DRIVER_FUNCTION HANDLE
-COM_Open(PVOID pInitContext, DWORD AccessCode, DWORD ShareMode) {
-	// XXX: not implemented
-	return NULL;
-}
-
-DRIVER_FUNCTION BOOL
-COM_Close(PVOID pOpenContext) {
-	// XXX: not implemented
-	return FALSE;
-}
-
-DRIVER_FUNCTION ULONG
-COM_Read(PVOID pOpenContext, PUCHAR pTargetBuffer, ULONG BufferLength) {
-	// XXX: not implemented
-	return 0;
-}
-
-DRIVER_FUNCTION ULONG
-COM_Write(PVOID pOpenContext, PUCHAR pSourceBytes, ULONG NumberOfBytes) {
-	// XXX: not implemented
-	return 0;
-}
-
-DRIVER_FUNCTION ULONG
-COM_Seek(PVOID pOpenContext, LONG Position, DWORD Type) {
-	// XXX: not implemented
-	return 0;
 }
 
 DRIVER_FUNCTION BOOL
 COM_PowerUp(PVOID pInitContext) {
-	// XXX: not implemented
 	return FALSE;
 }
 
 DRIVER_FUNCTION BOOL
 COM_PowerDown(PVOID pInitContext) {
-	// XXX: not implemented
 	return FALSE;
+}
+
+DRIVER_FUNCTION HANDLE
+COM_Open(PVOID pInitContext, DWORD AccessCode, DWORD ShareMode) {
+	return NULL;
+}
+
+DRIVER_FUNCTION BOOL
+Com_PreClose(PVOID pOpenContext) {
+	return FALSE;
+}
+
+DRIVER_FUNCTION BOOL
+COM_Close(PVOID pOpenContext) {
+	return FALSE;
+}
+
+DRIVER_FUNCTION ULONG
+COM_Read(PVOID pOpenContext, PUCHAR pTargetBuffer, ULONG BufferLength) {
+	return 0;
+}
+
+DRIVER_FUNCTION ULONG
+COM_Write(PVOID pOpenContext, PUCHAR pSourceBytes, ULONG NumberOfBytes) {
+	return 0;
+}
+
+DRIVER_FUNCTION ULONG
+COM_Seek(PVOID pOpenContext, LONG Position, DWORD Type) {
+	return 0;
 }
 
 DRIVER_FUNCTION BOOL
 COM_IOControl(PVOID pOpenContext, DWORD dwCode, PBYTE pBufIn,DWORD dwLenIn,
     PBYTE pBufOut, DWORD dwLenOut, PDWORD pdwActualOut) {
-	// XXX: not implemented
 	return FALSE;
 }
